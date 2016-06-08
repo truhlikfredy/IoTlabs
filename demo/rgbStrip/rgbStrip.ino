c02be096d699751d38f07b3044fb6eab777fa37a
Anton Krug <antonkrug@gmail.com>
2016-06-08 01:26:46 +0100

    Less frequent updates but with delta value for the microphone

--- demo/rgbStrip/rgbStrip.ino
+++ demo/rgbStrip/rgbStrip.ino
@@ -4,24 +4,45 @@
 #define PORT_LIGHT       A1
 #define PORT_TEMPERATURE A2
 
+#define MAX_INT          65535
+
 
 void setup() {
   Serial.begin(SERIAL_SPEED);
 }
 
 void loop() {
-  static unsigned int valueMicrophone;
-  static unsigned int valueLight;
-  static unsigned int valueTemperature;
+  static unsigned int valueMicrophone  = 0;
+  static unsigned int minMicrophone    = MAX_INT;
+  static unsigned int maxMicrophone    = 0;
+  static unsigned int valueLight       = 0;
+  static unsigned int valueTemperature = 0;
+  static unsigned int tick             = 0;
   
   valueMicrophone  = analogRead(PORT_MICROPHONE );
   valueLight       = analogRead(PORT_LIGHT      );
   valueTemperature = analogRead(PORT_TEMPERATURE);
-  
-  //output data for the processing GUI
-  Serial.print(valueMicrophone);
-  Serial.print(',');
-  Serial.print(  map(valueLight,         0, 700, 0, 100));
-  Serial.print(',');
-  Serial.println(map(valueTemperature, 400, 500, 0, 100));
+
+  if (valueMicrophone < minMicrophone) {
+    minMicrophone = valueMicrophone;
+  }
+
+  if (valueMicrophone > maxMicrophone) {
+    maxMicrophone = valueMicrophone;
+  }
+
+  if ((tick % 255) == 0 ) {
+    //output data for the processing GUI once in while
+    Serial.print(maxMicrophone - minMicrophone);
+    Serial.print(',');
+    Serial.print(  map(valueLight,         0, 700, 0, 100));
+    Serial.print(',');
+    Serial.println(map(valueTemperature, 400, 500, 0, 100));  
+
+    minMicrophone    = MAX_INT;
+    maxMicrophone    = 0;
+  }
+
+  tick++;
 }
+
