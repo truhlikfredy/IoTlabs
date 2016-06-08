d4ffa785ae73330b3ab73859a09691521c4adc72
Anton Krug <antonkrug@gmail.com>
2016-06-08 02:00:31 +0100

    Faster refresh, averaged results (less noise and jitter)

--- demo/rgbStrip/rgbStrip.ino
+++ demo/rgbStrip/rgbStrip.ino
@@ -1,10 +1,12 @@
-#define SERIAL_SPEED     115200
+#define SERIAL_SPEED       115200
 
-#define PORT_MICROPHONE  A0
-#define PORT_LIGHT       A1
-#define PORT_TEMPERATURE A2
+#define REPORT_AFTER_TICKS 128
 
-#define MAX_INT          65535
+#define PORT_MICROPHONE    A0
+#define PORT_LIGHT         A1
+#define PORT_TEMPERATURE   A2
+
+#define MAX_INT            65535
 
 
 void setup() {
@@ -12,12 +14,14 @@ void setup() {
 }
 
 void loop() {
-  static unsigned int valueMicrophone  = 0;
-  static unsigned int minMicrophone    = MAX_INT;
-  static unsigned int maxMicrophone    = 0;
-  static unsigned int valueLight       = 0;
-  static unsigned int valueTemperature = 0;
-  static unsigned int tick             = 0;
+  static unsigned int  valueMicrophone  = 0;
+  static unsigned int  minMicrophone    = MAX_INT;
+  static unsigned int  maxMicrophone    = 0;
+  static unsigned int  valueLight       = 0;
+  static unsigned long sumLight         = 0;
+  static unsigned int  valueTemperature = 0;
+  static unsigned long sumTemperature   = 0;
+  static unsigned int  tick             = 0;
   
   valueMicrophone  = analogRead(PORT_MICROPHONE );
   valueLight       = analogRead(PORT_LIGHT      );
@@ -31,16 +35,21 @@ void loop() {
     maxMicrophone = valueMicrophone;
   }
 
-  if ((tick % 255) == 0 ) {
+  sumTemperature += valueTemperature;
+  sumLight       += valueLight;
+
+  if ((tick % REPORT_AFTER_TICKS) == 0 ) {
     //output data for the processing GUI once in while
     Serial.print(maxMicrophone - minMicrophone);
     Serial.print(',');
-    Serial.print(  map(valueLight,         0, 700, 0, 100));
+    Serial.print(  map(sumLight / REPORT_AFTER_TICKS,         0, 700, 0, 100));
     Serial.print(',');
-    Serial.println(map(valueTemperature, 400, 500, 0, 100));  
+    Serial.println(map(sumTemperature / REPORT_AFTER_TICKS, 400, 500, 0, 100));  
 
     minMicrophone    = MAX_INT;
     maxMicrophone    = 0;
+    sumTemperature   = 0;
+    sumLight         = 0;
   }
 
   tick++;
